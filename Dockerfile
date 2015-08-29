8bf9f2085f66d297bea5cf2e5b348b814917d4b3
Oleg Morozenkov <omorozenkov@gmail.com>
2015-08-29 13:48:25 +0300

    Auto injecting env vars

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM nginx
 MAINTAINER Oleg Morozenkov
 
+COPY nginx-inject-env.sh /usr/libexec/nginx-inject-env.sh
 COPY nginx.conf /etc/nginx/nginx.conf
 RUN rm -rf /etc/nginx/sites-enabled && mkdir -p /etc/nginx/sites-enabled
+
+CMD ["bash", "/usr/libexec/nginx-inject-env.sh"]
---
+++ nginx-inject-env.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd /
+rm -rf /etc/nginx-inj
+cp -r /etc/nginx /etc/nginx-inj
+find /etc/nginx-inj -type f -print0 | while read -d $'\0' file
+do
+	printenv | while read line
+	do
+		envname=${line%=*}
+		envvalue=${line#*=}
+		echo $envname "@" $envvalue
+		sed -i "s|\$ENV_$envname|$envvalue|g" $file
+	done
+	sed -i "s|/etc/nginx|/etc/nginx-inj|g" $file
+done
+
+cd /etc/nginx-inj
+nginx -c /etc/nginx-inj/nginx.conf -g "daemon off;"
